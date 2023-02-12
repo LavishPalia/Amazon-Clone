344add06656a728f495bd700850ea8f4750e7a09
LavishPalia <66507561+LavishPalia@users.noreply.github.com>
2023-02-12 05:55:00 +0000

    Nodemailer code to send mail - forgot password

---
+++ utils/mailHelper.js
@@ -0,0 +1,14 @@
+import transporter from '../config/index.js';
+import config from '../config/index.js';
+
+const mailHelper = async (options) => {
+  const message = {
+    from: config.SMTP_MAIL_EMAIL, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.text, // plain text body
+    // html: '<b>Hello world?</b>', // html body
+  };
+
+  await transporter.sendMail(message);
+};
