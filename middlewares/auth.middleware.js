d3e8f12f754299075674e0dd0bd19e652c3ec697
LavishPalia <66507561+LavishPalia@users.noreply.github.com>
2023-03-10 16:45:40 +0000

    added isLoggedIn middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import User from '../models/user.schema.js'
+import JWT from 'jsonwebtoken'
+import asyncHandler from '../services/asyncHandler.js'
+import CustomError from '../utils/customError.js'
+import config from '../config/index.js'
+
+export const isLoggedIn = asyncHandler(async(req, _res, next) => {
+    let token;
+
+    if(req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))) {
+        token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    }
+
+    if(!token) {
+        throw new CustomError('Not authorized to access this route', 401);
+    }
+
+    try {
+        const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET)
+        req.user = await User.findById(decodedJwtPayload._id, "name email role");
+        next();
+    } catch (err) {
+        throw new CustomError('Not authorized to access this route', 401);
+    }
+})
